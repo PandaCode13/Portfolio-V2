80f154590630b0da9a4a7ea82088bd48cece8411
PANDACODE13 <smohamedabdo895@gmail.com>
2026-01-23 11:21:21 +0100

    Message clair

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,14 @@ function App() {
   return (
     <>
       <Header />
-      <Home />
-      <About />
-      <Skills />
-      <FormCertif />
-      <Projects />
-      <Contact />
+      <div className="main-conntent">
+        <Home />
+        <About />
+        <Skills />
+        <FormCertif />
+        <Projects />
+        <Contact />
+      </div>
       <Footer />
     </>
   )
